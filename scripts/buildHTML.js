429bbcf8df7b1f09c890516e5ee0a770c96ecf0e
Evan Norton <evan@retro-mmo.com>
2023-02-16 02:15:06 -0500

    fix js path for static

--- scripts/buildHTML.js
+++ scripts/buildHTML.js
@@ -1,6 +1,6 @@
 const { readFileSync, writeFileSync } = require("fs");
 const { join } = require("path");
 
-const html = readFileSync("template.mustache").toString().replace("data-run-id=\"{{runID}}\"", "");
+const html = readFileSync("template.mustache").toString().replace("data-run-id=\"{{runID}}\"", "").replace("./out/script.js", "./script.js");
 
 writeFileSync(join("out", "index.html"), html);
