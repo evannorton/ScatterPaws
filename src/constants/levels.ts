74a7f10aed3e7b981f1f2fa1ea894a332ea53763
Evan Norton <evan@retro-mmo.com>
2023-02-23 12:33:37 -0500

    levels

--- src/constants/levels.ts
+++ src/constants/levels.ts
@@ -2,16 +2,16 @@ import Level from "../interfaces/Level";
 
 const levels: Level[] = [
   {
-    startingTileX: 7,
-    startingTileY: 17,
+    startingTileX: 6,
+    startingTileY: 15,
     tilemapSlug: "map",
-    time: 20000
+    time: 30000
   },
   {
     startingTileX: -10,
     startingTileY: 36,
     tilemapSlug: "map-2",
-    time: 60000
+    time: 120000
   }
 ];
 
