e4dd849422c12253a68811d4810c3178e002c155
Evan Norton <evan@retro-mmo.com>
2023-02-24 06:40:26 -0500

    move bed HUD up

--- src/functions/draw/drawBedHUD.ts
+++ src/functions/draw/drawBedHUD.ts
@@ -4,7 +4,7 @@ import drawRectangle from "./drawRectangle";
 import drawText from "./drawText";
 
 const drawBedHUD = (): void => {
-  const offset: number = 4;
+  const offset: number = 8;
   drawRectangle("#000000", .5, gameWidth / 2 - 43, gameHeight / 2 - 26 - offset, 86, 15, 10004)
   drawText("Click to start!", "#ffffff", gameWidth / 2, gameHeight / 2 - 24 + 2 - offset, 1, gameWidth, 1, "center", "top");
 };
