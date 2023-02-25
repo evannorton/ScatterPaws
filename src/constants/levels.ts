8a0c7272ac6932ec635dfc56d21ecaf12f0beb3c
Evan Norton <evan@retro-mmo.com>
2023-02-24 22:30:04 -0500

    update maps

--- src/constants/levels.ts
+++ src/constants/levels.ts
@@ -4,27 +4,27 @@ const levels: Level[] = [
   {
     bed: 0,
     requiredDestructibles: 5,
-    startingTileX: 7,
-    startingTileY: 17,
+    startingTileX: 8,
+    startingTileY: 18,
     tilemapSlug: "map",
     time: 60000
   },
   {
     bed: 1,
-    requiredDestructibles: 13,
-    startingTileX: -10,
-    startingTileY: 36,
+    requiredDestructibles: 15,
+    startingTileX: -15,
+    startingTileY: 41,
     tilemapSlug: "map-2",
     time: 120000
   },
-  // {
-  // bed: 2,
-  //   requiredDestructibles: 20,
-  //   startingTileX: 37,
-  //   startingTileY: 17,
-  //   tilemapSlug: "map-3",
-  //   time: 120000
-  // }
+  {
+    bed: 2,
+    requiredDestructibles: 20,
+    startingTileX: 54,
+    startingTileY: -2,
+    tilemapSlug: "map-3",
+    time: 120000
+  },
 ];
 
 export default levels;
